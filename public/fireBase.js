bdd3192f6e05e2d2c4a5b9337f023419a1690d5b
Alexander Eyre <alexander.paul.eyre@gmail.com>
2019-04-16 21:13:48 +1000

    Complete redesign of the login screen v1

---
+++ public/fireBase.js
@@ -0,0 +1,19 @@
+var app_fireBase = {};
+(function(){
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDvNXC31lNmym8bywLU-5cQpj1r9Ff-rvs",
+    authDomain: "ruleslist-499ae.firebaseapp.com",
+    databaseURL: "https://ruleslist-499ae.firebaseio.com",
+    projectId: "ruleslist-499ae",
+    storageBucket: "ruleslist-499ae.appspot.com",
+    messagingSenderId: "709988376759"
+  };
+  firebase.initializeApp(config);
+
+  app_fireBase = firebase;
+
+})()
+
+
